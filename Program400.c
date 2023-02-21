f363b5d867321fdfb3cef511b190791cecf8682b
Barde Vishal Shivaji <95370589+Vishalshiavjibarde@users.noreply.github.com>
2023-02-21 20:23:26 +0530

    Add files via upload

---
+++ Program400.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("Number of argumnets are : %d\n",argc);   //argument count
+
+    printf("Name of executable is : %s\n",argv[0]);  //argument vector
+    
+    return 0;
+}
